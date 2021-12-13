9c13d77a271566c3d7bb12874d4e8a4e6b2427f8
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-12-13 13:04:29 +0500

    b

--- app/src/main/java/com/example/recyclerview/myRVA.java
+++ app/src/main/java/com/example/recyclerview/myRVA.java
@@ -1,24 +1,32 @@
 package com.example.recyclerview;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class myRVA extends RecyclerView.Adapter {
+public class myRVA extends RecyclerView.Adapter<myRVA.MyViewHolder> {
     List<friends> friendsList;
+
+
+
+    public myRVA(List<friends> friendsList) {
+        this.friendsList = friendsList;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
     }
 
@@ -27,7 +35,19 @@ public class myRVA extends RecyclerView.Adapter {
         return 0;
     }
 
-    public myRVA(List<friends> friendsList) {
-        this.friendsList = friendsList;
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageViewFriend;
+        TextView textViewFriendName;
+        TextView textViewdateFriend;
+        TextView textViewCityFriend;
+        friends data;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewFriend = itemView.findViewById(R.id.imageView);
+            textViewFriendName = itemView.findViewById(R.id.textView);
+            textViewdateFriend = itemView.findViewById(R.id.textView2);
+            textViewCityFriend = itemView.findViewById(R.id.textView3);
+        }
+
     }
 }
