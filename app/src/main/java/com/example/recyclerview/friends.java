34eb7607c49101a3f011ccbf2d266dcc815fd50c
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-12-13 12:38:31 +0500

    item layout

--- app/src/main/java/com/example/recyclerview/friends.java
+++ app/src/main/java/com/example/recyclerview/friends.java
@@ -5,6 +5,40 @@ public class friends {
     private String name;
     private int dob;
     private String city;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDob() {
+        return dob;
+    }
+
+    public void setDob(int dob) {
+        this.dob = dob;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
 
     public void friends(int i,String n,String c,int d)
     {
