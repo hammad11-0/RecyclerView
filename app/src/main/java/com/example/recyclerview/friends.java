62d061e3540c3034eaad38163cf03b2563bf1ec0
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-12-13 12:30:48 +0500

    constructor

---
+++ app/src/main/java/com/example/recyclerview/friends.java
@@ -0,0 +1,24 @@
+package com.example.recyclerview;
+
+public class friends {
+    private int id;
+    private String name;
+    private int dob;
+    private String city;
+
+    public void friends(int i,String n,String c,int d)
+    {
+        this.id=i;
+        this.name=n;
+        this.dob=d;
+        this.city=c;
+
+
+    }
+
+
+
+
+
+
+}
